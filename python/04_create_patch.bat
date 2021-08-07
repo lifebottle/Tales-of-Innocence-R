969cead484790906ada7238e119b4e9c86305b71
pnvnd <peter@datacrunch.ca>
2021-08-07 14:36:43 -0400

    Automation now creates .zip with patch files inside

--- python/04_create_patch.bat
+++ python/04_create_patch.bat
@@ -1,4 +1,5 @@
 xdelta.exe -f -e -s "PCSG00009_dec\eboot.bin" "patched\eboot.bin" "eboot.xdelta"
-xdelta.exe -f -e -s "eboot.bin.elf" "patched\eboot.bin" "fagdec_eboot_fagdec.xdelta"
+xdelta.exe -f -e -s "eboot.bin.elf" "patched\eboot.bin" "fagdec_eboot.xdelta"
 xdelta.exe -f -e -s "PCSG00009_dec\toidata_release.l7c" "orig-dir\toidata_release.l7c" "toidata_release.xdelta"
+tar.exe -acf toir_patch_new.zip eboot.xdelta fagdec_eboot.xdelta toidata_release.xdelta toir_readme.txt
 pause
