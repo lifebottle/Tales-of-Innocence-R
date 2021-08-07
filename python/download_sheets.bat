2314bc4537625169183ea171fe9dd82266e4a194
pnvnd <peter@datacrunch.ca>
2021-08-07 13:42:29 -0400

    Added script to download Translated Google Sheets

---
+++ python/download_sheets.bat
@@ -0,0 +1,7 @@
+REM Sheets with <space> in the name should be replaced with %20. Using BAT file, escape % by using %%20 instead.
+curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=Skits%%205.0" > ./translated/Skit.csv
+curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=Story%%201.0" > ./translated/Story.csv
+curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=TESTING-eboot" > ./translated/eboot.csv
+curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=Done-Artes" > ./translated/ArtsDataPack.csv
+curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=WIP-Items" > ./translated/ItemDataPack.csv
+pause
