2f09f22ed980955d2c7f7ff3d3e47d17640975bb
pnvnd <peter@datacrunch.ca>
2021-08-07 19:22:43 -0400

    Fixed date format for release tag

--- 05_release.bat
+++ 05_release.bat
@@ -1,3 +1,3 @@
-set tag=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2%
-gh release create %tag% C:\Users\pvnn\Desktop\ToIR_Patch\toir_patch.zip --title "Tales of Innocence R - English Patch %date%" --notes "Automated Release %date% %time%"
+set tag=%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%.%time:~0,2%%time:~3,2%
+gh release create %tag% C:\Users\pvnn\Desktop\ToIR_Patch\toir_patch.zip --title "Tales of Innocence R - English Patch %date% %time%" --notes "Automated Release %date% %time%"
 pause
