70a4b1f8607db47ef2905e24bcf86086f52569b7
pnvnd <peter@datacrunch.ca>
2021-08-07 19:10:39 -0400

    Fixed missing folder issue

--- python/01_download_sheets.bat
+++ python/01_download_sheets.bat
@@ -1,4 +1,5 @@
 REM Sheets with <space> in the name should be replaced with %20. Using BAT file, escape % by using %%20 instead.
+if not exist "translated" mkdir translated
 curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=Skits%%205.0" > ./translated/Skit.csv
 curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=Story%%201.0" > ./translated/Story.csv
 curl -L "https://docs.google.com/spreadsheets/d/1hfZIQXpGyQw6dQtG_oVKI7bkO0teIUG9bXN9kKrANBw/gviz/tq?tqx=out:csv&sheet=TESTING-eboot" > ./translated/eboot.csv
