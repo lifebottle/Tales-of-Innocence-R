dddf0880d7984b01b846bd2fcaca1c6f95620c78
pnvnd <peter@datacrunch.ca>
2021-08-07 13:57:33 -0400

    Updated monospace script (0x8630 change 0x80 to 0x40)

--- python/monospace.py
+++ python/monospace.py
@@ -1,11 +1,18 @@
 import sys
 
-def monospace():
+def monospace1():
     eboot = open('patched/eboot.bin', 'r+b')
     eboot.seek(0x7B5E)
     eboot.write(b'\x00\x23')
     eboot.close()
 
+def monospace2():
+    eboot = open('patched/eboot.bin', 'r+b')
+    eboot.seek(0x8630)
+    eboot.write(b'\x40\x18')
+    eboot.close()
+
 if __name__ == '__main__':
     if sys.argv[1] == '1':
-        monospace()
+        monospace1()
+        monospace2()
