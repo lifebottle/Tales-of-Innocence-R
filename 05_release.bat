d18f1314c1576fa6c063b63ebd17c47a481335be
pnvnd <peter@datacrunch.ca>
2021-08-19 13:04:39 -0400

    Updated BAT file

--- 05_release.bat
+++ 05_release.bat
@@ -1,3 +1,3 @@
 set tag=%DATE:~6,4%%DATE:~0,2%%DATE:~3,2%.%time:~0,2%%time:~3,2%
-gh release create %tag% C:\Users\pvnn\Desktop\ToIR_Patch\toir_patch.zip --title "Tales of Innocence R - English Patch %date% %time%" --notes "Automated Release %date% %time% Story 60%%, Skit 58%%, Menu 52%%, NPC 40%%"
+gh release create %tag% C:\Users\pvnn\Desktop\ToIR_Patch\toir_patch.zip --title "Tales of Innocence R - English Patch %date% %time%" --notes "Automated Release %date% %time% Story 62%%, Movie Subs 90%%, Skit 60%%, Menu 76%%, NPC 43%%"
 pause
