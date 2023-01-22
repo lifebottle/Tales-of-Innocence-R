513a95120a5f57831324e220975735039f9adc31
FistingUranus <77898683+Fis-Ura@users.noreply.github.com>
2023-01-22 17:10:14 -0500

    Update asm

--- tools/asm/armv7-a_macros.asm
+++ tools/asm/armv7-a_macros.asm
@@ -21,11 +21,51 @@
 
 ;################################
 ;
+;       Labels
+;
+;################################
+
+; For less akward macro writing I
+; globally define labels with the
+; same names as the register
+; (which apparently is valid?)
+; so we can refer to them the same
+; was as a normal instruction
+; No such thing for # and [] because
+; there's no token level parsing
+; only text replacements and some
+; string manipulation
+
+.definelabel r0, 0
+.definelabel r1, 1
+.definelabel r2, 2
+.definelabel r3, 3
+.definelabel r4, 4
+.definelabel r5, 5
+.definelabel r6, 6
+.definelabel r7, 7
+.definelabel r8, 8
+.definelabel r9, 9
+.definelabel r10, 10
+.definelabel r11, 11
+.definelabel r12, 12
+
+.definelabel r13, 13
+.definelabel r14, 14
+.definelabel r15, 15
+
+.definelabel sp, 13
+.definelabel lr, 14
+.definelabel pc, 15 
+
+;################################
+;
 ;         B instructions
 ;   Check A8.8.18 in the manual
 ;
 ;################################
 
+; encoding T3
 .macro b.w,dst_addr
     ; Not on Thumb mode, bail out!
     .if isthumb() != 1
@@ -52,6 +92,34 @@
            )
 .endmacro
 
+; encoding T3
+.macro bne.w,dst_addr
+    ; Not on Thumb mode, bail out!
+    .if isthumb() != 1
+        .error "bne.w T3 encoding error: Not on thumb mode!"
+    .endif
+
+    ; This is the T3 encoding of this instruction, 
+    ; so jumps are limited to -1048576 to 1048574 even jumps
+    .if getoff(., dst_addr) > 1048574 || getoff(., dst_addr) < -1048576
+        .error "bne.w T3 encoding error: label out of range"
+    .endif
+
+    ; the half words are swapped because it's in Thumb mode
+    ; also, not having variables makes this a clusterfuck
+    .word swaphw(\
+            /* Top bits */ (0b11110 << 27) | \
+            /* Sign */ ((getoff(., dst_addr) < 0 ? 1 : 0) << 26) | \
+            /* cond */ (0b0001 << 22)|\ /* NE */
+            /* Imm6 */ (((getoff(., dst_addr) >> 12) & 0b111111) << 16) | \
+            /* 1 */ (1 << 15) | \
+            /* J1 */ ((((~(((getoff(., dst_addr) & (1 << 23)) ? 1 : 0) & 1) ^ (getoff(., dst_addr) < 0)) & 1)) << 13) |\
+            /* 1 */ (0 << 12) | \ /* T3 encoding */
+            /* J2 */ ((((~((getoff(., dst_addr) & (1 << 22) ? 1 : 0) & 1) ^ (getoff(., dst_addr) < 0)) & 1)) << 11) | \
+            /* Imm11 */ (((getoff(., dst_addr) >> 1) & 0b11111111111))\
+           )
+.endmacro
+
 ;################################
 ;
 ;         MOV instructions
@@ -62,6 +130,9 @@
 ; mov T2 encoding, encodes a <const>
 ; ranges defined by A6.3.2 section of the manual
 .macro mov.w,rd,val
+    .ifndef r3
+        .error "!"
+    .endif
     ; Not on Thumb mode, bail out!
     .if isthumb() != 1
         .error "mov.w T2 encoding error: Not on thumb mode!"
@@ -178,9 +249,11 @@
 ;################################
 ;
 ;         STR instructions
-;    Check A8.8.204 in the manual
+;    Check A8.8 in the manual
 ;
 ;################################
+
+; STR Check A8.8.204
 
 ; str T3 encoding, encodes an <Imm12>
 .macro str.w,rt,rn,val
@@ -207,4 +280,16 @@
             /* Rt */ ((rt & 0xF) << 12) | \
             /* Imm12 */ (val & 0b111111111111) \
            )
+.endmacro
+
+; STRH Check A8.8.217
+
+; str T3 encoding, encodes an <Imm12> BROKEN
+.macro strh.w,rt,rn,val
+    ; Not on Thumb mode, bail out!
+    .if isthumb() != 1
+        .error "str.w T3 encoding error: Not on thumb mode!"
+    .endif
+
+    .error "Not implemented"
 .endmacro
