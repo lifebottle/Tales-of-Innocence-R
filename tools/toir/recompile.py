be6d22a9c14b5c7dfd58f191e15db4057830d029
FistingUranus <77898683+Fis-Ura@users.noreply.github.com>
2023-01-22 17:10:40 -0500

    Update recompile scripts

--- tools/toir/recompile.py
+++ tools/toir/recompile.py
@@ -23,13 +23,13 @@ def recompile(source, newtext, destination):
     destl7cdir = destination / 'toidata_release'
     destl7cdir.mkdir(parents=True, exist_ok=True)
 
-    #toir.recompile_eboot(ebootpath, newtext, destination)
+    toir.recompile_eboot(ebootpath, newtext, destination)
     toir.recompile_dat(l7cdir, newtext, destl7cdir)
-    #toir.recompile_skits(l7cdir, newtext, destl7cdir)
-    #toir.recompile_story(l7cdir, newtext, destl7cdir)
-    #toir.recompile_map_data(l7cdir, newtext, destl7cdir)
+    toir.recompile_skits(l7cdir, newtext, destl7cdir)
+    toir.recompile_story(l7cdir, newtext, destl7cdir)
+    toir.recompile_map_data(l7cdir, newtext, destl7cdir)
     toir.recompile_textures(l7cdir, newtext, destl7cdir)
-    #toir.insert_srt(newtext, destl7cdir)
+    toir.insert_srt(newtext, destl7cdir)
 
 if __name__ == '__main__':
     recompile()
