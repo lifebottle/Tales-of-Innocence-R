c0f583bd2139901d52be6b1b34057d4ab81dbc11
FistingUranus <77898683+Fis-Ura@users.noreply.github.com>
2023-10-21 16:11:29 -0500

    Update asm

--- tools/asm/InnocenceR.asm
+++ tools/asm/InnocenceR.asm
@@ -3,8 +3,60 @@
 .thumb
 .include "armv7-a_macros.asm"
 
+; fonts from app folder (broken)
+;.org 0x810059d2
+;movs r1,0x0
+;bl   0x81005340
+
+
+;################################
+;       100 save slots hack
+;################################
+
+; The save class has an array with
+; every slot it's goin to use (among other stuff)
+; at 0x20 bytes per slot it's 25 x 0x20 = 0x320
+; then at 100 slots we'd need 100 x 0x20 = 0xC80
+; So we just tack that at the end of the allocator
+; original size + the extra data is:
+; 0x6CC + 0xC80 = 0x134C
+.org 0x8100bdd6
+movw       r0, 0x134C
+
+; Move the slot array pointer to the end of the
+; allocated space (new space)
+.org 0x8101d950
+addw       r5,r4,0x6cc ; for save screen
+.org 0x8101d874
+addw       r5,r4,0x6cc ; for load screen
+
+; Update saveSlots array allocation, just memset
+; the extra memory :)
+.org 0x8101d95a
+movs.w     r2,0xC80 ; for save screen
+.org 0x8101d87e
+movs.w     r2,0x640 ; for load screen
+
+
+; Update saveSlots array init
+; For save screen
+.org 0x8101d9c2 :: str.w      r2,/*[*/r3,0x6cc/*]*/
+.org 0x8101d9c8 :: str.w      r1,/*[*/r3,0x6d8/*]*/
+.org 0x8101d9ce :: cmp        r2, 100
+; Same but for load screen
+.org 0x8101d8e6 :: str.w      r0,/*[*/r2,0x6cc/*]*/
+.org 0x8101d8ee :: cmp        r0, 100
+
+; Update array size field
+.org 0x8101d8f8 :: movs.w     lr, 100
+.org 0x8101d9d8 :: movs.w     lr, 100
+
+;################################
+;     Button alignment fix
+;################################
+
 .org 0x8102ABBE
-cmp        r3,#0x40
+cmp        r3,0x40
 bne        skip
 b.w        tag_case
 
@@ -37,6 +89,11 @@ add.w      r7,lr,0x4
 add.w      r8,r2,0x2
 b.w        default
 
+; Change sceCommonDialogSetConfigParam function caller
+; to use the system language in the save screen
+.org 0x8100bdd0
+movs       r1,0x1
+
 
 ; Cooking Lv text overlaps the number
 ; so let's move that to the left
@@ -45,11 +102,13 @@ movw       r2,0x154
 
 
 ; Remove the monospacing 0x01 is on, 0x00 is off
-.orga 0x7B5E
-.byte 0x00
+.org 0x81005b5e
+movs r3,0x0
 
 ; Change the width of the whitespace to half from 0x80 to 0x40
-.orga 0x8630
+.org 0x81006630
+; .org 0x8100662E :: add.w      r8,r8,r0, lsl #0x6
+
 .byte 0x40
 
 
@@ -63,5 +122,31 @@ movw       r2,0x154
 ; from the japanese one, so let's use 2
 .org 0x8115CF70
 .asciiz "  "
+
+; ProcField setIsMonospace = false
+.org 0x810C502E
+movs r1,/*#*/0x00
+
+; ProcBattle setIsMonospace = false
+.org 0x8103F356
+movs r1,/*#*/0x00
+.org 0x8103F364
+movs r1,/*#*/0x00
+
+; font raster thing (breaks notice, I think)
+.org 0x810063E0
+.byte 0xb1, 0xf8, 0x66, 0x10 ;ldrh.w     r1,[r1,#0x66]
+
+.org 0x8100664E
+movs r1,/*#*/0x00 ; og .byte 0xf1, 0x69 ldr        r1,[r6,#0x1c]
+
+
+
+; Style screen setIsMonospace = false
+.org 0x8111E99E
+movs r1,/*#*/0x00
+
+.org 0x8111E9C2
+movw r1,/*#*/0x00 ; movw r1,/*#*/0x209
 
 .close
--- tools/asm/armv7-a_macros.asm
+++ tools/asm/armv7-a_macros.asm
@@ -15,8 +15,36 @@
 ;
 ;################################
 
+.expfunc getbyte(value, n), ((value >> (8 * (n - 1))) & 0xff)
 .expfunc getoff(src_addr, dst_addr), ((dst_addr & (~1)) - (src_addr & (~1)) - 4)
 .expfunc swaphw(w), (w >> 16 | w << 16)
+.expfunc clz(x), x == 0 ? 32 : clz_1(x, 0xFFFF0000, 32)
+
+.expfunc clz_1(x, val, k),\
+    (k == 0) ? \ 
+        0 \
+    : \
+        ((x & val) == 0) ? (\
+            (k >> 1) + clz_1((x << (k >> 1)), (val << (k >> 2)), (k >> 1)) \
+        )\
+        : (\
+            0 + clz_1(x, (val << (k >> 2)), (k >> 1))\
+        )
+
+.expfunc ctz(x), x == 0 ? 32 : ctz_1(x, 0x0000FFFF, 32)
+
+.expfunc ctz_1(x, val, k), \
+    (k == 0) ? (\ 
+        0 \
+    )\
+    : (\
+        ((x & val) == 0) ? (\
+            (k >> 1) + ctz_1((x >> (k >> 1)), (val >> (k >> 2)), (k >> 1)) \
+        )\
+        : (\
+            0 + ctz_1(x, (val >> (k >> 2)), (k >> 1))\
+        )\
+    )
 
 
 ;################################
@@ -127,6 +155,14 @@
 ;
 ;################################
 
+; add T1 encoding, encodes an <Imm8>
+.macro movs,rd,val
+    ; According to D8.1 this should be the same
+    ; as mov rd,val in ARMv6k (3DS) and ARMv4T (GBA)
+    ; so just pop that in
+    mov rd,val
+.endmacro
+
 ; mov T2 encoding, encodes a <const>
 ; ranges defined by A6.3.2 section of the manual
 .macro mov.w,rd,val
@@ -166,6 +202,89 @@
            )
 .endmacro
 
+
+; mov T2 encoding, encodes a <const>
+; ranges defined by A6.3.2?? section of the manual
+.macro movs.w,rd,val
+    ; Not on Thumb mode, bail out!
+    .if isthumb() != 1
+        .error "movs.w T2 encoding error: Not on thumb mode!"
+    .endif
+
+    ; As per the manual, can only address the first 13 GPR
+    ; not that you could write "SP" or "LR" in a macro...
+    .if rd < 0 || rd == 13 || rd == 15
+        .warning "movs.w T2 encoding warning: Invalid target register, bahavior is UNPREDICTABLE"
+    .endif
+
+    .if val <= 0xFF || val == 0
+        ; the half words are swapped because it's in Thumb mode
+        ; Top bits are just saying "Yep, this is a thumb instruction"
+        ; i = 0, we only encode 0xFF bytes
+        ; S = 1, this is movs.w not mov.w
+        ; Imm3 = 0, we only encode 0xFF bytes
+        .word swaphw(\
+            /* Top bits */ (0b11110 << 27) | \
+            /* i */ (0 << 26) | \
+            /* st1 */ (0b00010 << 21) | \
+            /* S */ (1 << 20) | \
+            /* st2 */ (0b11110 << 15) |\
+            /* Imm3 */ (0b000 << 12) | \
+            /* Rd */ ((rd & 0xF) << 8) | \
+            /* Imm8 */ (val & 0b11111111)\
+        )
+    .elseif val == (((val & 0xFF) << 16) | (val & 0xFF))
+        .word swaphw(\
+            /* Top bits */ (0b11110 << 27) | \
+            /* i */ (0 << 26) | \
+            /* st1 */ (0b00010 << 21) | \
+            /* S */ (1 << 20) | \
+            /* st2 */ (0b11110 << 15) |\
+            /* Imm3 */ (0b001 << 12) | \
+            /* Rd */ ((rd & 0xF) << 8) | \
+            /* Imm8 */ (val & 0b11111111)\
+        )
+    .elseif val == (((val & 0xFF) << 24) | ((val & 0xFF) << 8))
+        .word swaphw(\
+            /* Top bits */ (0b11110 << 27) | \
+            /* i */ (0 << 26) | \
+            /* st1 */ (0b00010 << 21) | \
+            /* S */ (1 << 20) | \
+            /* st2 */ (0b11110 << 15) |\
+            /* Imm3 */ (0b010 << 12) | \
+            /* Rd */ ((rd & 0xF) << 8) | \
+            /* Imm8 */ (val & 0b11111111)\
+        )
+    .elseif val == (((val & 0xFF) << 24) | ((val & 0xFF) << 16) | ((val & 0xFF) << 8) | (val & 0xff))
+        .word swaphw(\
+            /* Top bits */ (0b11110 << 27) | \
+            /* i */ (0 << 26) | \
+            /* st1 */ (0b00010 << 21) | \
+            /* S */ (1 << 20) | \
+            /* st2 */ (0b11110 << 15) |\
+            /* Imm3 */ (0b011 << 12) | \
+            /* Rd */ ((rd & 0xF) << 8) | \
+            /* Imm8 */ (val & 0b11111111)\
+        )
+    .elseif (32 - abs(clz(val) + ctz(val))) <= 8
+        .word swaphw(\
+            /* Top bits */ (0b11110 << 27) | \
+            /* i */ (((clz(val) >> 4) & 0b1) << 26) | \
+            /* st1 */ (0b00010 << 21) | \
+            /* S */ (1 << 20) | \
+            /* st2 */ (0b11110 << 15) |\
+            /* Imm3 */ (((0b100 + (clz(val) >> 1)) & 0b111) << 12) | \
+            /* Rd */ ((rd & 0xF) << 8) | \
+            /* A */ (((clz(val) % 2) & 0b1) << 7) | \
+            /* Imm8 */ (((val >> ctz(val)) << (8 - (32 - abs(clz(val) + ctz(val))))) & 0b1111111)\
+        )
+    .else
+        .error "Can't encode constant for movs.w"
+    .endif
+
+    
+.endmacro
+
 ; movw T3 encoding, encodes an <Imm16>
 .macro movw,rd,val
     ; Not on Thumb mode, bail out!
@@ -183,9 +302,9 @@
     ; the Imm16 is encoded as | Imm4 : i : Imm3 : Imm8 |
     .word swaphw(\
             /* Top bits */ (0b11110 << 27) | \
-            /* i */ (((val >> 11) & 0b00001) << 26) | \
+            /* i */ (((val >> 11) & 0b1) << 26) | \
             /* st1 */ (0b100100 << 20) | \
-            /* Imm4 */ (((val >> 12) & 0b1111) << 15) |\
+            /* Imm4 */ (((val >> 12) & 0b1111) << 16) |\
             /* st2 */ (0b0 << 15) |\
             /* Imm3 */ (((val >> 8) & 0b111) << 12) | \
             /* Rd */ ((rd & 0xF) << 8) | \
@@ -240,6 +359,38 @@
             /* Rn */ ((rn & 0xF) << 16) | \
             /* st2 */ (0b0 << 15) |\
             /* Imm3 */ (0b000 << 12) | \
+            /* Rd */ ((rd & 0xF) << 8) | \
+            /* Imm8 */ (val & 0b11111111)\
+           )
+.endmacro
+
+; add T4 encoding, encodes an <Imm12>
+; ranges defined by A8.8.4 section of the manual
+.macro addw,rd,rn,val
+    ; Not on Thumb mode, bail out!
+    .if isthumb() != 1
+        .error "add.w T4 encoding error: Not on thumb mode!"
+    .endif
+
+    ; As per the manual, can only address the first 13 GPR
+    ; not that you could write "SP" or "LR" in a macro...
+    .if rd == 13 || rn == 15 || rd == 15
+        .warning "add.w T4 encoding warning: Invalid source or target register, bahavior is UNPREDICTABLE"
+    .endif
+
+    ; the half words are swapped because it's in Thumb mode
+    ; Top bits are just saying "Yep, this is a thumb instruction"
+    ; i = 0, we only encode 0xFF bytes
+    ; S = 0, this is add.w not adds.w
+    ; Imm3 = 0, we only encode 0xFF bytes
+    .word swaphw(\
+            /* Top bits */ (0b11110 << 27) | \
+            /* i */ (((val >> 11) & 0b00001) << 26) | \
+            /* st1 */ (0b10000 << 21) | \
+            /* S */ (0 << 20) | \
+            /* Rn */ ((rn & 0xF) << 16) | \
+            /* st2 */ (0b0 << 15) |\
+            /* Imm3 */ (((val >> 8) & 0b111) << 12) | \
             /* Rd */ ((rd & 0xF) << 8) | \
             /* Imm8 */ (val & 0b11111111)\
            )
